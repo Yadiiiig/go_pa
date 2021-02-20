e9f0606b27b1c6e75177ce3edda1a0a1dde949d8
yadiiiig <willemteughels@gmail.com>
2021-02-20 18:20:17 +0100

    Non-Auth request will now get append to denylist

--- api/auth.go
+++ api/auth.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 )
 
@@ -12,6 +13,7 @@ func authenticationCheck(request http.HandlerFunc) http.HandlerFunc {
 			if auth == authKey {
 				request(w, r)
 			} else {
+				nonAuthRequest(r.RemoteAddr)
 				w.WriteHeader(403)
 				json.NewEncoder(w).Encode("What are you trying to accomplish?")
 			}
@@ -20,4 +22,51 @@ func authenticationCheck(request http.HandlerFunc) http.HandlerFunc {
 			json.NewEncoder(w).Encode("What are you trying to accomplish?")
 		}
 	})
+}
+
+func nonAuthRequest(ip string) {
+	returnedIP := selectedIP{}
+
+	rows, err := db.Queryx("SELECT ip, tries, blocked FROM denylist WHERE ip = ?", ip)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for rows.Next() {
+		err := rows.StructScan(&returnedIP)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
+	rows.Close()
+
+	if returnedIP.IP == "" {
+		_, err := db.Query("INSERT INTO denylist (ip, tries) VALUES (?, 1)", ip)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	} else if returnedIP.Tries != 4 {
+		_, err := db.Query("UPDATE denylist SET tries = ? WHERE ip = ?", returnedIP.Tries+1, ip)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	} else {
+		_, err := db.Query("UPDATE denylist SET tries = ?, blocked = 1 WHERE ip = ?", returnedIP.Tries+1, ip)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		// Add code to add IP to map
+	}
+
+}
+
+type selectedIP struct {
+	IP      string `db:"ip"`
+	Tries   int    `db:"tries"`
+	Blocked int    `db:"blocked"`
 }
